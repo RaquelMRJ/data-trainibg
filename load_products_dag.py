b16af5d88607e926b4ee24787a45a9ec5c75495e
RaquelMRJ <97487302+RaquelMRJ@users.noreply.github.com>
2022-01-16 23:21:25 -0600

    Update load_products_dag.py

--- load_products_dag.py
+++ load_products_dag.py
@@ -24,9 +24,9 @@ welcome_operator = PythonOperator(task_id='welcome_task',
 s3_to_postgres_operator = S3ToPostgresTransfer(
                             task_id = 'dag_s3_to_postgres',
                             schema =  'bootcampdb', #'public'
-                            table= 'products',
+                            table= 'user_purchase',
                             s3_bucket = 's3-data-bootcamp',
-                            s3_key =  'products.csv',
+                            s3_key =  'user_purchase.csv',
                             aws_conn_postgres_id = 'postgres_default',
                             aws_conn_id = 'aws_default',   
                             dag = dag

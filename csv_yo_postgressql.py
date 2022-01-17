b5f5746ed136dabb4fc3d1a3f6ecaeb595309de6
RaquelMRJ <97487302+RaquelMRJ@users.noreply.github.com>
2022-01-17 14:52:05 -0600

    Update csv_yo_postgressql.py

--- csv_yo_postgressql.py
+++ csv_yo_postgressql.py
@@ -44,7 +44,7 @@ def csv_to_postgres():
     get_postgres_conn = PostgresHook(postgres_conn_id='postgres_default').get_conn()
     curr = get_postgres_conn.cursor()
     # CSV loading to table
-    with open(file_path("/custom_modules/assets/cities_clean.csv"), "r") as f:
+    with open(file_path("/custom_modules/user_purchase.csv"), "r") as f:
         next(f)
         curr.copy_from(f, 'cities', sep=",")
         get_postgres_conn.commit()
